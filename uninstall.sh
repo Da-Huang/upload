8c1e6531f6a5f62c15733be90a2cd1ac59c9beb5
Da Huang <dhuang.cn@gmail.com>
2016-07-05 21:40:31 +0800

    Reloaded apache2 after dissite.

--- uninstall.sh
+++ uninstall.sh
@@ -4,3 +4,4 @@ sudo a2dissite exchange upload
 sudo unlink /etc/apache2/sites-available/exchange.conf
 sudo unlink /etc/apache2/sites-available/upload.conf
 sudo rm -rf /usr/share/upload
+sudo service apache2 reload
