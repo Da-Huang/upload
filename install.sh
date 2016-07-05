22acefc531619c1ffdd9954988654ad0a593de70
Da Huang <dhuang.cn@gmail.com>
2016-07-05 21:34:44 +0800

    Fix username.

--- install.sh
+++ install.sh
@@ -2,18 +2,21 @@
 
 CURRENT_PATH=`realpath $0 | xargs dirname`
 
-sed -i "s/{USER}/$USER/g" conf/exchange.conf upload.py
+REAL_USER=`echo ~ | xargs basename`
+
+sed -i "s/{USER}/$REAL_USER/g" conf/exchange.conf upload.py
 mkdir ~/Exchange
 sudo chmod 777 ~/Exchange
 
 sudo apt-get install apache2 libapache2-mod-python
-sudo cp -r CURRENT_PATH /usr/share
+sudo cp -r $CURRENT_PATH /usr/share
 
 cd /usr/share/upload
 
-sudo rm /etc/apache2/sites-available/exchange.conf
-sudo rm /etc/apache2/sites-available/upload.conf
-sudo ln -s /usr/share/upload/conf/*.conf /etc/apache2/sites-available
+sudo rm -f /etc/apache2/sites-available/exchange.conf
+sudo ln -s /usr/share/upload/conf/exchange.conf /etc/apache2/sites-available
+sudo rm -f /etc/apache2/sites-available/upload.conf
+sudo ln -s /usr/share/upload/conf/upload.conf /etc/apache2/sites-available
 
 sudo a2ensite exchange upload
 sudo a2enmod cgid
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo rm -r /usr/share/upload
+sudo rm -rf /usr/share/upload
 sudo a2dissite exchange upload
-sudo rm /etc/apache2/sites-available/exchange.conf
-sudo rm /etc/apache2/sites-available/upload.conf
+sudo unlink /etc/apache2/sites-available/exchange.conf
+sudo unlink /etc/apache2/sites-available/upload.conf
