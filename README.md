14055584afc4b13f032c49c58f6dabb0ae960f99
Da Huang <dhuang.cn@gmail.com>
2016-07-05 21:15:16 +0800

    Moved configures to conf.
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+# Exchange - Upload
+This tool aims to provide downloading and uploading file services.
+
+---
+# Requirements
+* Ubuntu >= 12.04
+
+---
+# Installation
+```bash
+sudo ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+CURRENT_PATH=`realpath $0 | xargs dirname`
+
+#sudo apt-get install apache2 libapache2-mod-python
+
+sed -i "s/{USER}/$USER/g"
+
+sudo rm /etc/apache2/sites-available/exchange.conf
+sudo rm /etc/apache2/sites-available/upload.conf
+sudo ln -s $CURRENT_PATH/conf/*.conf /etc/apache2/sites-available
+
+#sudo a2ensite exchange upload
+#sudo a2enmod cgid
+#sudo service apache2 restart
