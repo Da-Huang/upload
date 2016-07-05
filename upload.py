f3964717d48099e964d375c21ee4ec4273d4d377
Da Huang <dhuang.cn@gmail.com>
2016-07-05 20:40:39 +0800

    Made it workable.

--- upload.py
+++ upload.py
@@ -5,20 +5,23 @@ import cgitb
 cgitb.enable()
 
 import cgi, os
-def download(item):
+
+def Download(item):
   name = os.path.basename(item.filename)
   path = os.path.join('/home/sewm/Exchange', name)
-  if os.path.exists(path): return 'fail for %s, caused by name conflicts' % name
-  out = open(path, 'w')
-  out.write(item.file.read())
-  out.close()
-  os.chmod(path, 0x777)
-  return 'successful for %s!' % name
+  if os.path.exists(path):
+    return 'Fail for %s, caused by name conflicts' % name
+  with open(path, 'w') as out:
+    out.write(item.file.read())
+    os.chmod(path, 0x777)
+  return 'Successful for %s!' % name
 
 print 'Content-Type: text/html\n\n'
 form = cgi.FieldStorage()
 files = form['file']
 
 if isinstance(files, list):
-  for item in files: print download(item) + '<br/>'
-else: print download(files) + '<br/>'
+  for item in files:
+    print Download(item) + '<br />'
+else:
+  print Download(files) + '<br />'
