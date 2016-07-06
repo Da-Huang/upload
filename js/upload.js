330cd3a8d6b6a1607d3feba2e6b729b703eef3da
Da Huang <dhuang.cn@gmail.com>
2016-07-06 21:00:50 +0800

    Added progress bar

--- js/upload.js
+++ js/upload.js
@@ -1,29 +1,52 @@
 $(document).ready(function() {
   $('.choose-file').change(function() {
+    $('.file-info-item:not(.template)').remove();
     var filenames = '';
     var spantext = $(this).parent().find('span');
     if (this.files && this.files.length > 0) {
       var info = $('.file-info');
       var item = $('.file-info-item.template');
+      var i = 0;
       $.each(this.files, function(i, file) {
-        info.append(item.clone().removeClass('template').text(file.name));
+        item = item.clone().removeClass('template');
+        item.attr('id', 'file-info-item-' + i++);
+        item.find('.filename').text(file.name);
+        info.append(item);
       });
-    } else {
-      $('.file-info-item:not(template)').remove();
     }
   });
 
-  $('#upload-file').click(function() {
+  $('.upload-file').click(function() {
     $('.choose-file').each(function() {
       $.each(this.files, function(i, file) {
+        var data = new FormData();
+        data.append('file', file);
         $.ajax({
           url: '/upload/upload.py',
-          data: {'file': file},
-          processData: false,
+          data: data,
           type: 'post',
-          contentType: 'multipart/form-data; charset=utf8',
+          processData: false,
+          contentType: false,
+          xhr: function() {
+            myXhr = $.ajaxSettings.xhr();
+            if (myXhr.upload){
+              myXhr.upload.addEventListener('progress',
+                  function(evt) {
+                    if (evt.lengthComputable) {
+                      var percentComplete = (evt.loaded / evt.total) * 100;
+                      var pb = $('#file-info-item-' + i).find('.progress-bar');
+                      pb.attr('aria-valuenow', percentComplete);
+                      pb.attr('style', 'width:' + percentComplete + '%;');
+                      pb.text(Math.round(percentComplete) + '%');
+                    }
+                  }, false);
+            } else {
+              console.log('Uploadress is not supported.');
+            }
+            return myXhr;
+          }
         }).done(function(data) {
-          alert(data);
+          console.log(data);
         });
       });
     });
